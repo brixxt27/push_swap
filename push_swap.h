31e6af9bb5b5a1a1415db1a85f144f2dbb5a4e8b
Jaesang Yoon <jayoon@c5r5s7.42seoul.kr>
2022-06-30 14:50:05 +0900

    edit: README.md
    README 작성하고, 앞으로 어떻게 프로그램을 작성해 나갈지 계획을 세워봤다.
    jim 님의 학장님과의 식사 썰을 듣고, 앞으로 어떻게 공부해 나갈지 다시금
    되세기는 시간을 갖고 다시 이 프로젝트를 보니 처음부터 잘못 접근했다는
    것을 깨달았다. 앞으로는 main.c 부터 작성할 것이다.

--- push_swap.h
+++ push_swap.h
@@ -6,17 +6,25 @@
 /*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 22:11:19 by jayoon            #+#    #+#             */
-/*   Updated: 2022/06/28 22:13:27 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/06/29 21:19:27 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_list
+typedef struct s_node
 {
-    int             value;
-    struct s_list   *next;
-}   t_list;
+	int				value;
+	struct s_list	*prev;
+	struct s_list	*next;
+}	t_node;
+
+typedef struct s_linked_list
+{
+	int		num_node;
+	t_node	*head;
+	t_node	*tail;
+}	t_list;
 
 #endif
