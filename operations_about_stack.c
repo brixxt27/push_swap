37109ea36d2bc9425abc9be9f2902285c262a28b
brixxt27 <brixxt27@gmail.com>
2022-07-18 22:09:36 +0900

    feat: list, stack operations
    push 완성했고, pop 구현하다가 hyko님 구현에 하나만 있을 때를 처리해주지
    않아 관련 문의를 넣었다. 이후에 구현 마무리 해야 한다.

--- operations_about_stack.c
+++ operations_about_stack.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 21:05:27 by jayoon            #+#    #+#             */
-/*   Updated: 2022/07/16 19:13:29 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/07/18 22:09:03 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,29 @@ void	init_stack(t_stack *ps)
 	ps->bottom = NULL;
 }
 
-// ps->top == NULL
 void	first_push(t_stack *ps, t_node *node)
 {
 	ps->top = node;
 	ps->bottom = node;
 	ps->size = 1;
+	node->next = node;
+	node->prev = node;
+}
 
+void	push(t_stack *ps, t_node *node)
+{
+	node->next = ps->top;
+	ps->top->prev = node;
+	node->prev = ps->bottom;
+	ps->bottom->next = node;
+	ps->top = node;
+	ps->size++;
+}
+
+t_node	pop(t_stack *ps)
+{
+	t_node	*temp;
+
+	temp = ps->top;
+	ps->bottom->next = ps->top->next;
 }
