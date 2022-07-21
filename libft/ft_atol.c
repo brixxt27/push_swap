1a167ce3b773bb4b4cb1d434d251f4d632db4c9f
brixxt27 <brixxt27@gmail.com>
2022-07-21 20:59:06 +0900

    comment: ft_atol.c
    부호가 하나라도 들어가면 0을 반환한다... 왜 이러는지 모르겠어서 다시
    구현해야 한다. 갈 길이 멀었구나 ^^

--- libft/ft_atol.c
+++ libft/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 15:09:43 by jayoon            #+#    #+#             */
-/*   Updated: 2022/07/21 20:26:22 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/07/21 20:51:26 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,9 @@ ssize_t	ft_atol(const char *str)
 	str = pass_space(str);
 	str = check_sign(str, &sign);
 	len = do_atol(str, &result);
-	if (len > 10)
-		return ((ssize_t)INT_MAX + 1);
+	// if (len > 19 && sign == 1)
+	// 	return ((ssize_t)INT_MAX + 1);
+	// else if (len > 19 && sign == -1)
+	// 	return ((ssize_t)INT_MIN - 1);
 	return ((ssize_t)sign * result);
 }
