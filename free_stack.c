ac49a5a04aa31b018ff4360dc9d80a09d879d0da
brixxt27 <brixxt27@gmail.com>
2022-07-22 15:45:26 +0900

    refactor: free_stack.c
    AddressSanitizer: heap-use-after-free on address 0x603000001788 at pc 0x0001033386b8 bp 0x7ffeec8ca940 sp 0x7ffeec8ca938
    temp 에 다음 노드의 주소를 미리 담아두고, free 를 해서 해결했다.

--- free_stack.c
+++ free_stack.c
@@ -6,7 +6,7 @@
 /*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 21:49:28 by jayoon            #+#    #+#             */
-/*   Updated: 2022/07/21 21:51:54 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/07/22 15:36:26 by jayoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	free_stack(t_stack *a)
 {
 	t_node	*curr;
+	t_node	*temp;
 
 	curr = a->top;
 	while (curr)
 	{
+		temp = curr->next;
 		free(curr);
-		curr = curr->next;
+		curr = temp;
 	}
 	free(a);
 }
